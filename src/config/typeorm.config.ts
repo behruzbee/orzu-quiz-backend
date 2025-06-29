475cf103f5ed4956ef845d258f851230a3ae0734
Behruz <behruzbahtiyorov10@gmail.com>
2025-06-30 04:32:48 +0500

    setup database

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,14 +2,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
 export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => {
-  const db = configService.get('database');
+  // const db = configService.get('database');
   return {
     type: 'mysql',
-    host: db.host,
-    port: db.port,
-    username: db.username,
-    password: db.password,
-    database: db.name,
+    host: "mysql.railway.internal",
+    port: 3306,
+    username: "root",
+    password: "qNucmsUnkzBYhWEqMTDknysCvdBbvILn",
+    database: "railway",
     autoLoadEntities: true,
     synchronize: true,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
